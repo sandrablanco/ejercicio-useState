e65db8ac73180dd8db1dc487d6fba5f56fefea92
Sandra Blanco <sandra.blanco18@gmail.com>
2026-02-25 10:05:21 +0100

    updating App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react';
 import ReactDom from 'react-dom/client';
 
+
 function App() {
 const [name, setName] = useState("Danel");
+const [newName, setnewName] = useState("");
+
+
+const changeName = () =>{
+  if (newName.trim() !== "") {
+  setName(newName);
+  setNewName("");
+ }
+}
 
   return (
     <div>
@@ -13,6 +23,10 @@ const [name, setName] = useState("Danel");
         <li onClick={() => setName("Reyes")}>Reyes</li>
         <li onClick={() => setName("Soraya")}>Yolanda</li>
       </ul>
+      <input type="text" value={newName} onChange={(e) => setnewName(e.target.value)} placeholder="add a name" />
+        <button type="button" onClick={changeName}>
+        Add
+      </button>
     </div>
   );
 }
